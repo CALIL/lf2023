a95db7c9f6c71a41ececd8fc22eb1aa937fad7b0
Satoshi Deguchi <deguchik@gmail.com>
2020-10-08 11:07:24 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-console.log('Hello LooselyTemplate!')
-
 fetch('https://blog.calil.jp/wp-json/wp/v2/posts/?categories=108').then(function(r) { return r.json() }).then(function(r) {
     console.log(r)
     var div = document.getElementById('whatsnew');

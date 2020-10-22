7422b529c6f915e4d132016adff6f6ad17d62580
Satoshi Deguchi <deguchik@gmail.com>
2020-10-22 11:59:13 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,12 @@ fetch('https://blog.calil.jp/wp-json/wp/v2/posts/?categories=108').then(function
     console.log(r)
     var div = document.getElementById('whatsnew');
     r.map(function(item) {
+        var p = document.createElement('p');
         var a = document.createElement('a');
         a.href = item.link;
         a.target = '_blank';
         a.innerHTML = item.title.rendered;
-        div.appendChild(a);
+        p.appendChild(a)
+        div.appendChild(p);
     })
 });

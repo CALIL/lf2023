73a944b39dffaaf7baca13559e702527aae49ba7
deguchi <deguchik@gmail.com>
2021-10-01 11:44:07 +0900

    update replace_path

--- replace_path.js
+++ replace_path.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-fs.readFile('./docs/index.html', 'utf8', function (err, data) {
+fs.readFile('./build/index.html', 'utf8', function (err, data) {
     if (err) {
         return console.log(err);
     }
@@ -7,7 +7,7 @@ fs.readFile('./docs/index.html', 'utf8', function (err, data) {
     var result = data.replace(/\.\/header\.css/g, 'https://calil.github.io/lf2020/header.css');
     result = result.replace(/\.\.\/assets\//g, 'https://calil.github.io/lf2020/assets/');
 
-    fs.writeFile('./docs/index.html', result, 'utf8', function (err) {
+    fs.writeFile('./build/index.html', result, 'utf8', function (err) {
         if (err) return console.log(err);
     });
 });

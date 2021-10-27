3dc8590a757d8d9e46d15ca67fea3d4a4ac193d7
deguchi <deguchik@gmail.com>
2021-10-27 10:02:21 +0900

    update

--- replace_path.js
+++ replace_path.js
@@ -17,8 +17,8 @@ fs.readFile('./build/header.html', 'utf8', function (err, data) {
     if (err) {
         return console.log(err);
     }
-    var result = data.replace(/\/assets\/header\.css/g, 'https://calil.github.io/lf2021/assets/header.css');
-    result = result.replace(/\/assets\/header\.js/g, 'https://calil.github.io/lf2021/assets/header.js');
+    var result = data.replace(/\/lf2021\/assets\/header\.css/g, 'https://calil.github.io/lf2021/assets/header.css');
+    result = result.replace(/\/lf2021\/assets\/header\.js/g, 'https://calil.github.io/lf2021/assets/header.js');
     result = result.replace(/\.\/assets\//g, 'https://calil.github.io/lf2021/assets/');
 
     fs.writeFile('./build/header.html', result, 'utf8', function (err) {

f4c1ac8e99241e0f4ed0264d5dfa2f6925971efa
deguchi <deguchik@gmail.com>
2021-10-27 10:06:25 +0900

    update

--- replace_path.js
+++ replace_path.js
@@ -3,10 +3,10 @@ fs.readFile('./build/index.html', 'utf8', function (err, data) {
     if (err) {
         return console.log(err);
     }
-    // var result = data.replace(/\/assets\/main\.css/g, 'https://calil.github.io/lf2021/assets/main.css');
-    // result = result.replace(/\/assets\/main\.js/g, 'https://calil.github.io/lf2021/assets/main.js');
-    var result = data.replace(/\.\/src\/js\/index\.js/g, '/lf2021/js/index.js');
-    // result = result.replace(/\.\/assets\//g, 'https://calil.github.io/lf2021/assets/');
+    var result = data.replace(/\/lf2021\/assets\/main\.css/g, 'https://calil.github.io/lf2021/assets/main.css');
+    result = result.replace(/\/lf2021\/assets\/main\.js/g, 'https://calil.github.io/lf2021/assets/main.js');
+    result = result.replace(/\.\/src\/js\/index\.js/g, 'https://calil.github.io/lf2021/js/index.js');
+    result = result.replace(/\.\/assets\//g, 'https://calil.github.io/lf2021/assets/');
 
     fs.writeFile('./build/index.html', result, 'utf8', function (err) {
         if (err) return console.log(err);

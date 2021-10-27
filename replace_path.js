d6b0132bf40e09de80c1d1a5ddaf45e944e4fdec
deguchi <deguchik@gmail.com>
2021-10-27 10:04:43 +0900

    update

--- replace_path.js
+++ replace_path.js
@@ -5,7 +5,7 @@ fs.readFile('./build/index.html', 'utf8', function (err, data) {
     }
     // var result = data.replace(/\/assets\/main\.css/g, 'https://calil.github.io/lf2021/assets/main.css');
     // result = result.replace(/\/assets\/main\.js/g, 'https://calil.github.io/lf2021/assets/main.js');
-    var result = result.replace(/\\.\/src\/js\/index\.js/g, 'https://calil.github.io/lf2021/js/index.js');
+    var result = data.replace(/\.\/src\/js\/index\.js/g, '/lf2021/js/index.js');
     // result = result.replace(/\.\/assets\//g, 'https://calil.github.io/lf2021/assets/');
 
     fs.writeFile('./build/index.html', result, 'utf8', function (err) {

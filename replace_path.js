ab3ef1f8d3e7a1b7793a3d37857d1206e25511a8
Satoshi Deguchi <deguchik@gmail.com>
2023-10-13 11:30:22 +0900

    update

--- replace_path.js
+++ replace_path.js
@@ -3,7 +3,7 @@ fs.readFile('./build/index.html', 'utf8', function (err, data) {
     if (err) {
         return console.log(err);
     }
-    var result = data.replace(/\/lf2023\/assets\/main\.css/g, 'https://calil.github.io/lf2023/assets/main.css');
+    var result = data.replace(/\/lf2023\/assets\/index\.css/g, 'https://calil.github.io/lf2023/assets/index.css');
     result = result.replace(/\/lf2023\/assets\/main\.js/g, 'https://calil.github.io/lf2023/assets/main.js');
     result = result.replace(/\.\/src\/js\/index\.js/g, 'https://calil.github.io/lf2023/js/index.js');
     result = result.replace(/\.\/assets\//g, 'https://calil.github.io/lf2023/assets/');
